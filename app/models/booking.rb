ed037f7a1ca5ebe4de0402b95b41867e46cf5bcb
luis zhang <luischn94@gmail.com>
2019-07-25 13:19:15 -0300

    career view professional view booking view

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,9 +1,9 @@
 class Booking < ApplicationRecord
-  has_many :reviews
   has_many :messages, dependent: :destroy
   belongs_to :professional
   belongs_to :user
   enum booking_status: {pending: 0, confirmed: 1, declined: 2}
   validates :date, presence: true
-  validates :intro_message, presence: true, length: { minimum: 20 }
+  validates :intro_message, presence: true
+  has_many :reviews
 end
