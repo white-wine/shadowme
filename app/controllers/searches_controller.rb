ebe437d94836d1a8b1bdd38f93909ea0b55d4e17
soledad escobar <m.sol.escobar@gmail.com>
2019-07-30 19:28:48 -0300

    navbar search and cards

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,11 +2,11 @@ class SearchesController < ApplicationController
    skip_before_action :authenticate_user!
   def index
     if params[:search_type] == "career"
-      @careers = Career.search_by_title(params[:q])
+      @results = Career.search_by_title(params[:q])
     elsif params[:search_type] == "category"
-      @categories = Category.search_by_title(params[:q])
+      @results = Category.search_by_title(params[:q])
     elsif params[:search_type] == "user"
-      @users = User.search_by_name_or_specialty(params[:q]).where(user_type: 1)
+      @results = User.search_by_name_or_specialty(params[:q]).where(user_type: 1)
      else
        @results = PgSearch.multisearch(params[:q])
      end
