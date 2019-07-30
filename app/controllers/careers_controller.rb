6239800187b596fa4f9c96bc705fddbb45c07501
luis zhang <luischn94@gmail.com>
2019-07-30 19:25:41 -0300

    footer pink

--- app/controllers/careers_controller.rb
+++ app/controllers/careers_controller.rb
@@ -4,6 +4,14 @@ class CareersController < ApplicationController
   def show
     @career = Career.find(params[:id])
     @professionals = Professional.where(career: @career)
+    @agriculture_tags = %w(Freelance, Executive, Office Workder, Part-time, Entepreneur, farm, organic, research, growth, nature)
+    @education_tags = %w(Freelance, Executive, Office Workder, Part-time, Entepreneur, school, college, teacher, students, children)
+    @art_tags = %w(Freelance, Executive, Office Workder, Part-time, Entepreneur, paitings, design, style, artist, gallery, creative, photography, nature)
+    @engineer_tags = %w(Freelance, Executive, Office Workder, Part-time, Entepreneur, software, hardware, science, computer, development, project, electricity)
+    @health_tags = %w(Freelance, Executive, Office Workder, Part-time, Entepreneur, nutrition, healthcare, biology,)
+    @law_tags = %w(Freelance, Executive, Office Workder, Part-time, Entepreneur,)
+    @science_tags = %w(Freelance, Executive, Office Workder, Part-time, Entepreneur,)
+    @technology_tags = %w(Freelance, Executive, Office Workder, Part-time, Entepreneur,)
   end
 
 end
