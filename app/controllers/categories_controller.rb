4388c2866be256d7f234e30b20799d66934d664a
soledad escobar <m.sol.escobar@gmail.com>
2019-08-01 17:45:27 -0300

    change colors and weight of font

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,14 +9,14 @@ before_action :set_category, only: [:show]
   def show
     @careers = Career.where(category:@category)
     @tags = {
-            "agriculture" => "Freelance, Executive, Office Worker, Part-time, Entepreneur, Farm, Organic, Research, Growth, Nature",
-            "education" => "Freelance, Executive, Office Worker, Part-time, Entepreneur, School, College, Teacher, Students, Children",
-            "arts" => "Freelance, Executive, Office Worker, Part-time, Entepreneur, Paintings, Design, Style, Artist, Gallery, Creative, Photography, Nature",
-            "engineering" => "Freelance, Executive, Office Worker, Part-time, Entepreneur, Software, hardware, Science, Computer, Development, Project, Electricity",
-            "health" => "Freelance, Executive, Office Worker, Part-time, Entepreneur, Nutrition, Genetic, Lab, Healthcare, Biology,",
-            "law" => "Freelance, Executive, Office Worker, Part-time, Entepreneur, Criminally, Juridical, Legal, Politics, Courts",
-            "science" => "Freelance, Executive, Office Worker, Part-time, Entepreneur, Cell, Biology, Phisical, Theory, Electricity",
-            "technology" => "Freelance, Executive, Office Worker, Part-time, Entepreneur, Date, Startup, Robotics, Software, Physic, Electricity"
+            "agriculture" => "Freelance, Executive, Office Worker, Part-time, Entrepreneur, Farm, Organic, Research, Growth, Nature",
+            "education" => "Freelance, Executive, Office Worker, Part-time, Entrepreneur, School, College, Teacher, Students, Children",
+            "arts" => "Freelance, Executive, Office Worker, Part-time, Entrepreneur, Paintings, Design, Style, Artist, Gallery, Creative, Photography, Nature",
+            "engineering" => "Freelance, Executive, Office Worker, Part-time, Entrepreneur, Software, hardware, Science, Computer, Development, Project, Electricity",
+            "health" => "Freelance, Executive, Office Worker, Part-time, Entrepreneur, Nutrition, Genetic, Lab, Healthcare, Biology,",
+            "law" => "Freelance, Executive, Office Worker, Part-time, Entrepreneur, Criminally, Juridical, Legal, Politics, Courts",
+            "science" => "Freelance, Executive, Office Worker, Part-time, Entrepreneur, Cell, Biology, Phisical, Theory, Electricity",
+            "technology" => "Freelance, Executive, Office Worker, Part-time, Entrepreneur, Date, Startup, Robotics, Software, Physic, Electricity"
           }
   end
 
